362a8d308a0233a4610a671eaeecb6d9185753a3
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2021-08-14 16:16:19 +0530

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
+BOT_TOKEN = "1840298314:AAFUMtMNiJpyBBt4tyGfuq_yO3ZXl88jxwk"
+API_ID = 5119765
+API_HASH = "ab310ff746864c1a33f3c590f1598c06"
+USERBOT_PREFIX = "."
+PHONE_NUMBER = "+16465640536"  # Need for Userbot  # Sudo users have full access to everything, don't trust anyone
+LOG_GROUP_ID = -100125431255
+GBAN_LOG_GROUP_ID = -1001263664495
+MESSAGE_DUMP_CHAT = -1001263664495
+FERNET_ENCRYPTION_KEY = "iKMq0WZMnJKjMQxZWKtv-cplMuF_LoyshXj0XbTGGWM="  # Leave this as it is
+WELCOME_DELAY_KICK_SEC = 300
+MONGO_DB_URI = "mongodb+srv://Satyal:lsatya@cluster0.vym2c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+ARQ_API_KEY = "NFXKWF-UYMFGH-OVWYFN-VXDNSM-ARQ"
+ARQ_API_URL = "https://thearq.tech"
+LOG_MENTIONS = True
+RSS_DELAY = 300  # In seconds
+PM_PERMIT = False
